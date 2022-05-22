122223b68cc4652f700b1e6efa38e098f9038812
Murrrrr <41497682+Murrrrr@users.noreply.github.com>
2022-05-23 01:12:46 +0900

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,14 +1,26 @@
-const express = require('express');
+const http = require("http");
+const express = require("express");
+const path = require("path");
+
 const app = express();
-const path = require('path');
-const http = require('http');
 
-const port = 8000;
+const port = 8000; //인스턴스 생성시 만들었던 포트번호 기입
 
-app.get("/", (req, res)=>{
-    res.send("pong");
-})
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
 
-app.listen(port, ()=>{
-    console.log("123");
-})
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("/*", (req, res) => {
+  res.set({
+    "Cache-Control": "no-cache, no-store, must-revalidate",
+    Pragma: "no-cache",
+    Date: Date.now()
+  });
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+http.createServer(app).listen(port, () => {
+  console.log(`app listening at ${port}`);
+});
